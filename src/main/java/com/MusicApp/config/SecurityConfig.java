5cc76f38ac6896f02d16321178b0897bbc91d424
Aboje Edwin <abojeedwin@gmail.com>
2025-10-02 20:05:30 +0100

    Added frontend to cors

--- src/main/java/com/MusicApp/config/SecurityConfig.java
+++ src/main/java/com/MusicApp/config/SecurityConfig.java
@@ -39,14 +39,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList(
-                "http://localhost:3000",
-                "https://*.vercel.app",
-                "https://*.onrender.com"
-        ));
+
+        configuration.addAllowedOriginPattern("*");
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;

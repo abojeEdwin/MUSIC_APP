5ca77590daf8e3ce92bea8e4cb5ffbd16676442d
Aboje Edwin <abojeedwin@gmail.com>
2025-10-03 23:38:54 +0100

    Added tomcat because for TLS

--- src/main/java/com/MusicApp/config/SecurityConfig.java
+++ src/main/java/com/MusicApp/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.ForwardedHeaderFilter;
 
 import java.util.Arrays;
 
@@ -53,10 +52,5 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
-    }
-
-    @Bean
-    public ForwardedHeaderFilter forwardedHeaderFilter() {
-        return new ForwardedHeaderFilter();
     }
 }

b5cb1e571f894d4480807e6a9da3d5000b3febce
Aboje Edwin <abojeedwin@gmail.com>
2025-10-13 13:47:18 +0100

    Added fronend depolyed url

--- src/main/java/com/MusicApp/config/WebConfig.java
+++ src/main/java/com/MusicApp/config/WebConfig.java
@@ -4,19 +4,16 @@ package com.MusicApp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("https://musicappfrontend.vercel.app")
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowCredentials(true);
     }
-
 }
